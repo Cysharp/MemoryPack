9e04321502b6ae7fba2ae3778864b8afff0fd223
neuecc <ils@neue.cc>
2022-09-07 04:26:22 +0900

    attributes

--- src/MemoryPack.Core/Attributes.cs
+++ src/MemoryPack.Core/Attributes.cs
@@ -12,14 +12,47 @@ public sealed class MemoryPackableAttribute : Attribute
 }
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true, Inherited = false)]
-public sealed class UnionAttribute : Attribute
+public sealed class MemoryPackUnionAttribute : Attribute
 {
     public byte Tag { get; set; }
     public Type Type { get; set; }
 
-    public UnionAttribute(byte tag, Type type)
+    public MemoryPackUnionAttribute(byte tag, Type type)
     {
         this.Tag = tag;
         this.Type = type;
     }
+}
+
+// similar naming as System.Text.Json attribtues
+// https://docs.microsoft.com/en-us/dotnet/api/system.text.json.serialization.jsonattribute
+
+[AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+public sealed class MemoryPackConstructorAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class MemoryPackOnSerializing : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class MemoryPackOnSerialized : Attribute
+{
+}
+
+//[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+//public class MemoryPackOnDeserializing : Attribute
+//{
+//}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class MemoryPackOnDeserialized : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public sealed class MemoryPackIgnoreAttribute : Attribute
+{
 }
