dd94916e3920d82c0a16c806224ee19b89fd34c5
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-09-28 16:19:34 +1000

    Update MemoryPackReader.cs

--- src/MemoryPack.Core/MemoryPackReader.cs
+++ src/MemoryPack.Core/MemoryPackReader.cs
@@ -196,7 +196,7 @@ public ref partial struct MemoryPackReader
         }
         if (length == 0)
         {
-            return string.Empty;
+            return "";
         }
 
         var byteCount = length * 2;
