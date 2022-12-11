8c70dc43e6e0492012f6dfa19941168637bcf413
neuecc <ils@neue.cc>
2022-12-11 23:45:57 +0900

    u

--- src/MemoryPack.Unity/Assets/Scripts/SampleOne.cs
+++ src/MemoryPack.Unity/Assets/Scripts/SampleOne.cs
@@ -1,6 +1,4 @@
-﻿#nullable disable
-
-using MemoryPack;
+﻿using MemoryPack;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,61 +19,4 @@ public partial class MyPerson
 {
     public int Age { get; set; }
     public string Name { get; set; }
-}
-
-#nullable enable
-
-[MemoryPackable]
-public readonly partial struct SerializableAnimationCurve
-{
-    [MemoryPackIgnore]
-    public readonly AnimationCurve AnimationCurve;
-
-    [MemoryPackInclude]
-    WrapMode preWrapMode => AnimationCurve.preWrapMode;
-    [MemoryPackInclude]
-    WrapMode postWrapMode => AnimationCurve.postWrapMode;
-    [MemoryPackInclude]
-    Keyframe[] keys => AnimationCurve.keys;
-
-    [MemoryPackConstructor]
-    SerializableAnimationCurve(WrapMode preWrapMode, WrapMode postWrapMode, Keyframe[] keys)
-    {
-        var curve = new AnimationCurve(keys);
-        curve.preWrapMode = preWrapMode;
-        curve.postWrapMode = postWrapMode;
-        this.AnimationCurve = curve;
-    }
-
-    public SerializableAnimationCurve(AnimationCurve animationCurve)
-    {
-        this.AnimationCurve = animationCurve;
-    }
-}
-
-public class AnimationCurveFormatter : MemoryPackFormatter<AnimationCurve>
-{
-    // Unity does not support scoped and TBufferWriter so change signature to `Serialize(ref MemoryPackWriter writer, ref AnimationCurve value)`
-    public override void Serialize(ref MemoryPackWriter writer, scoped ref AnimationCurve? value)
-    {
-        if (value == null)
-        {
-            writer.WriteNullObjectHeader();
-            return;
-        }
-
-        writer.WritePackable(new SerializableAnimationCurve(value));
-    }
-
-    public override void Deserialize(ref MemoryPackReader reader, scoped ref AnimationCurve? value)
-    {
-        if (reader.PeekIsNull())
-        {
-            value = null;
-            return;
-        }
-
-        var wrapped = reader.ReadPackable<SerializableAnimationCurve>();
-        value = wrapped.AnimationCurve;
-    }
 }
